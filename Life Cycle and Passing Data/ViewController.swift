b4c464ad1f15a25175b6540184eabd0b1cea022e
MohamedAdelAbbas <m.3del95@gmail.com>
2020-06-01 06:55:09 +0200

    Initial Commit

---
+++ Life Cycle and Passing Data/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Life Cycle and Passing Data
+//
+//  Created by Mohamed Adel on 6/1/20.
+//  Copyright © 2020 Mohamed Adel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
