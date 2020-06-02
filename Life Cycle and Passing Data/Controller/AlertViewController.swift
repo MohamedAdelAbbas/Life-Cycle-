499bb7b8fd1435047e111c1ef0d9d3315ed5eca4
MohamedAdelAbbas <m.3del95@gmail.com>
2020-06-02 08:33:10 +0200

    Add Alert Style

--- Life Cycle and Passing Data/Controller/AlertViewController.swift
+++ Life Cycle and Passing Data/Controller/AlertViewController.swift
@@ -12,19 +12,49 @@ class AlertViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+//    func showAlert() {
+//        let alert = UIAlertController(title: "Session 2", message: "Welcome to session 2", preferredStyle: .alert)
+//        self.present(alert, animated: true, completion: nil)
+//    }
+//
+//    func showAlertWithActions() {
+//        let alert = UIAlertController(title: "Session 2", message: "Are you sure you want to be an iOS engineer?", preferredStyle: .alert)
+//        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+//        let deleteAction = UIAlertAction(title: "Delete", style: .destructive, handler: { _ in print("Delete")
+//        })
+//        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive, handler: { _ in print("cancel")
+//        })
+//        alert.addAction(okAction)
+//        alert.addAction(deleteAction)
+//        alert.addAction(cancelAction)
+//        self.present(alert, animated: true, completion: nil)
+//    }
+//
+//    func showAlertWithActionSheet() {
+//        let alert = UIAlertController(title: "Session 2", message: "Are you sure you want to delete this?", preferredStyle: .actionSheet)
+//        let deleteAction = UIAlertAction(title: "Delete", style: .destructive, handler: { _ in print("Delete")})
+//        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: { _ in print("cancel")})
+//        alert.addAction(cancelAction)
+//        alert.addAction(deleteAction)
+//        self.present(alert, animated: true, completion: nil)
+//    }
+//
+    func showAlertWithTextField() {
+        let alert = UIAlertController(title: "Name Field", message: "Please enter your name", preferredStyle: .alert)
+        let doneAction = UIAlertAction(title: "Done", style: .default, handler: { _ in
+            let username = alert.textFields?[0].text
+            print("The username is... \(username ?? "--")")
+        })
+        alert.addAction(doneAction)
+        alert.addTextField(configurationHandler: { textField in
+            textField.placeholder = "Enter your name"
+        })
+        self.present(alert, animated: true, completion: nil)
     }
-    */
 
+    @IBAction func presentAlertPressed(_ sender: Any) {
+        showAlertWithTextField()
+    }
 }
